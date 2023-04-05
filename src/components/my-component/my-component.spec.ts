9d403071b005d3bcf00b31b9d707b06550332841
Christina Mardyla <christina.mardyla@relativity.com>
2023-04-05 14:07:49 -0500

    Add one more attempt.

--- src/components/my-component/my-component.spec.ts
+++ src/components/my-component/my-component.spec.ts
@@ -1,9 +1,15 @@
+const mockFoo = jest.fn();
+// Uncomment the below lines to test out the "third attempt" unit test
+// jest.mock("../my-other-component/my-other-component", () => ({
+//    foo: mockFoo
+// }));
+
 import { newSpecPage } from '@stencil/core/testing';
 import { MyOtherComponent } from '../my-other-component/my-other-component';
 import { MyComponent } from './my-component';
 
 describe('my-component', () => {
-  it("foo() calls foo on MyOtherComponent", async () => {
+  it("First attempt - foo() calls foo on MyOtherComponent", async () => {
     const page = await newSpecPage({
       components: [MyComponent, MyOtherComponent],
       html: `<my-component></my-component>`
@@ -13,14 +19,14 @@ describe('my-component', () => {
     const myOtherComponent = myComponent.shadowRoot.querySelector("my-other-component");
 
     // This errors with
-    //  TypeError: Cannot assign to read only property 'foo' of object '[object Object]'
+    // TypeError: Cannot assign to read only property 'foo' of object '[object Object]'
     const fooSpy = jest.spyOn(myOtherComponent, "foo");
 
     await myComponent.foo();
     expect(fooSpy).toHaveBeenCalled();
   });
 
-  it("foo() calls foo on MyOtherComponent", async () => {
+  it("Second attempt - foo() calls foo on MyOtherComponent", async () => {
     const fooSpy = jest.fn();
 
     const page = await newSpecPage({
@@ -43,5 +49,18 @@ describe('my-component', () => {
     const myComponent = page.root as HTMLMyComponentElement;
     await myComponent.foo();
     expect(fooSpy).toHaveBeenCalled();
+  });
+
+  it("Third attempt - foo() calls foo on MyOtherComponent", async () => {
+    // This line errors with
+    // TypeError: Cannot read properties of undefined (reading 'COMPILER_META')
+    const page = await newSpecPage({
+      components: [MyComponent, MyOtherComponent],
+      html: `<my-component></my-component>`
+    });
+
+    const myComponent = page.root as HTMLMyComponentElement;
+    await myComponent.foo();
+    expect(mockFoo).toHaveBeenCalled();
   });
 });
